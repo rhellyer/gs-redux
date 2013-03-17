5d34c4b6343f9fd8a1d7b05a929502040f1f5b7c
Richard Hellyer <richardhellyer@gmail.com>
2013-03-17 16:39:23 -0700

    field reports : edits
    added intro para
    included link to metrodigi

--- site/templates/field-reports.php
+++ site/templates/field-reports.php
@@ -4,28 +4,35 @@
 	<h1 class="title span12"><?php echo html($page->title()) ?></h1>
 </div>
 <div class="content row field-reports">
-	 
+
 	
-		<div class="row">
-			<div class="span9">
-				<h2 class="span12">Liberia </h2>
-				<p class="byline span12">March 2013</p>
-			</div>
-				<div class="span3 instructions">Click photos to view galleries</div>
+	<div class="row">
+		<div class="span9">
+			<h2 class="span12">Liberia </h2>
+			<p class="byline span12">March 2013</p>
 		</div>
-		  <?php foreach($page->images() as $image): ?>
-		  	<div class="article span6 clearfix">
-		  		<div class="image">
-		  			<a href="<?php echo $image->url() ?>" class="fancybox" rel="gallery1" title="<?php echo $image->title() ?>"><?php echo thumb($image, array('width' => 130, 'crop' => true)) ?></a></div>
-		  		<div class="caption"><?php echo $image->caption() ?></div>
-		  	</div>
-		  	
-		  <?php endforeach ?>
 		
+	</div>
+
+	<div class="span12">
+		<p style="font-style:italic">In a unique public/private partnership, Global Strategies has teamed up with <br>leading technology 
+			company <a href="http://www.metrodigi.com/">Metrodigi</a> to improve the care of neonates.  <br>Below are photos from our recent field test in Liberia<br><span style="font-size:12px">(click photos to view gallery)</span>
+		</p>
+	</div>
+
+
+	<?php foreach($page->images() as $image): ?>
+	<div class="article span6 clearfix">
+		<div class="image">
+			<a href="<?php echo $image->url() ?>" class="fancybox" rel="gallery1" title="<?php echo $image->title() ?>"><?php echo thumb($image, array('width' => 130, 'crop' => true)) ?></a></div>
+			<div class="caption"><?php echo $image->caption() ?></div>
+		</div>
+
+	<?php endforeach ?>
+
 </div>
-	
+
 
 
 
 <?php echo snippet('ending') ?>
-            
