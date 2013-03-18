b476390b2877abbda0bd39a2ca060724fbeb800f
Richard Hellyer <richardhellyer@gmail.com>
2013-03-18 00:27:37 -0700

    news articles: added list of articles in right col
    temporary solution - this list is hard coded

--- site/templates/newsarticle.php
+++ site/templates/newsarticle.php
@@ -5,16 +5,10 @@
 	<h1 class="title span12"><?php echo html($page->title()) ?></h1>
 </div>
 <div class="content row">
-	<div class="span12 annotation">
-		<?php echo kirbytext($page->annotation()); ?>
-	</div>
-	<div class="span2 leftcolumn">
-		<?php echo kirbytext($page->leftcolumn()); ?>
-	</div>
-	<div class="article span7 maincolumn">
+	<div class="article span8 maincolumn">
 		<?php echo kirbytext($page->text()) ?>
 	</div>
-	<div class="article span2 offset1 rightcolumn">
+	<div class="article span3 offset1 rightcolumn">
 		<?php echo kirbytext($page->rightcolumn()) ?>
 	</div>
 </div>
