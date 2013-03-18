f94dfc045a778804ce7900cbf4ac6a4d7499c004
Richard Hellyer <richardhellyer@gmail.com>
2013-03-17 22:45:44 -0700

    added analytics

--- site/snippets/ending.php
+++ site/snippets/ending.php
@@ -4,6 +4,13 @@
         <div class="wrapperwide footer">
             <div class="wrapper footer"></div>
         </div>
+
+<script type="text/javascript" src="http://www.google-analytics.com/ga.js"></script> 
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-4375610-18");
+pageTracker._trackPageview();
+} catch(err) {}</script>
         
         
     </body>
