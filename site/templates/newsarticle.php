83d3ec9e9b5c7b45c2f54923d8173467004d7ddb
Richard Hellyer <richardhellyer@gmail.com>
2013-03-18 23:08:05 -0700

    added dynamic sidebar to newsartice template

--- site/templates/newsarticle.php
+++ site/templates/newsarticle.php
@@ -1,6 +1,6 @@
 
 <?php echo snippet('beginning') ?>
-
+	
 <div class="row">
 	<h1 class="title span12"><?php echo html($page->title()) ?></h1>
 </div>
@@ -9,8 +9,23 @@
 		<?php echo kirbytext($page->text()) ?>
 	</div>
 	<div class="article span3 offset1 rightcolumn">
+		
+		<h3>Latest News</h2>
+		<ul style="list-style:none; margin:0; padding:0">
+			<?php $articles = $page->siblings()->visible()->flip(); 
+			foreach ($articles as $article) : ?>
+
+			<li>
+				&raquo; <a href="<?php echo $article->url() ?>"><?php echo $article->title() ?></a>
+			</li>
+			
+			<?php endforeach; ?>
+		</ul>
+
 		<?php echo kirbytext($page->rightcolumn()) ?>
+	
 	</div>
+
 </div>
 
 
