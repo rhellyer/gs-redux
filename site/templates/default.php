b8602b9dc1e13cb2f4d3dd29c9abf07a1bb45e21
Richard Hellyer <richardhellyer@gmail.com>
2013-03-20 10:27:00 -0700

    changed default to allow for 'no title' option

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,21 +1,20 @@
 <?php echo snippet('beginning') ?>
 
-<div class="row">
-	<div class="annotation">
-		<?php echo $page->annotation() ?>
+<?php if (!$page->hidetitle()) : ?> 
+	<div class="row">
+		<div class="span12">
+			<h1 class="title"><?php echo $page->title() ?></h1>
+		</div>
 	</div>
-</div>
-
-<div class="row">
-	<h1 class="title span12"><?php echo html($page->title()) ?></h1>
-</div>
+	<?php endif ?>
 <div class="content row">
 	 <div class="span1 leftcolumn">
 		<?php echo kirbytext($page->leftcolumn()); ?>
 	</div> 
 	<div class="article span6 maincolumn">
-		<div class="intro"><?php echo kirbytext($page->intro()) ?></div>
-		<?php echo kirbytext($page->text()) ?>
+		<div class="intro">
+			<?php echo kirbytext($page->intro()) ?></div>
+			<?php echo kirbytext($page->text()) ?>
 	</div>
 	<div class="article span5  rightcolumn">
 		<?php echo kirbytext($page->rightcolumn()) ?>
