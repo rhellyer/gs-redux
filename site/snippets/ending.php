86174c1f0d0d876e651d59bb0ceee8862556caef
Richard Hellyer <richardhellyer@gmail.com>
2013-03-21 14:35:34 -0700

    updated footer

--- site/snippets/ending.php
+++ site/snippets/ending.php
@@ -2,7 +2,7 @@
        		
     	</div><!-- end of wrapper div for content -->
         <div class="wrapperwide footer">
-            <div class="wrapper footer"> &copy; Copyright 2013 Global Strategies - (415) 451-1814 - info@globalstrategies.org</div>
+            <div class="wrapper footer">  Copyright &copy; 2013 Global Strategies - (415) 451-1814 - info@globalstrategies.org</div>
         </div>
 
 <script type="text/javascript" src="http://www.google-analytics.com/ga.js"></script> 
